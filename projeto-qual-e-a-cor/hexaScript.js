eabd42d250ae181cf1727dce673ab66aaf77d3a4
Pontinn <leo.pontin2@gmail.com>
2024-08-09 11:51:08 -0300

    Subindo App Cores Aleatórias

---
+++ projeto-qual-e-a-cor/hexaScript.js
@@ -0,0 +1,20 @@
+//Colors by Hexa
+
+const hexa = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F']
+
+const btnHexa = document.querySelector('.change-color-button-hexa')
+
+btnHexa.addEventListener('click', function() {
+
+    let hexaColor = '#'
+
+    for(i = 0; i < 6; i++){
+
+        hexaColor += hexa[Math.floor(Math.random() * hexa.length)]
+
+    }
+    
+    document.querySelector('.color-code').textContent = hexaColor
+    document.body.style.backgroundColor = hexaColor
+
+})
